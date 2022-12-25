a9aed33584118ec3f0cd01c214b067d940757c3a
kibethh <kibethh@gmail.com>
2022-12-25 05:18:47 +0000

    init commit

---
+++ src/graphql/queries.js
@@ -0,0 +1,37 @@
+import { request, gql } from "graphql-request";
+
+const GRAPHQL_URL = `http://localhost:9000/graphql`;
+
+export async function getJob(id) {
+  const query = gql`
+    query JobQuery($id: ID!) {
+      job(id: $id) {
+        id
+        title
+        company {
+          id
+          name
+        }
+        description
+      }
+    }
+  `;
+  const variables = { id };
+  const { job } = await request(GRAPHQL_URL, query, variables);
+  return job;
+}
+export async function getJobs() {
+  const query = gql`
+    query {
+      jobs {
+        id
+        title
+        company {
+          name
+        }
+      }
+    }
+  `;
+  const { jobs } = await request(GRAPHQL_URL, query);
+  return jobs;
+}
